2d85ee295bb47176212bf45629fa308aa8d978ae
Tim Koopmans <tim.koops@gmail.com>
2020-08-23 13:25:58 +1000

    ok, got it now

--- lib/utils.bash
+++ lib/utils.bash
@@ -86,11 +86,8 @@ install_version() {
     download_release "$version" "$release_file"
     tar -xzf "$release_file" -C "$install_path" || fail "Could not extract $release_file"
     rm "$release_file"
-    ls -alt "$install_path/"
 
-    local tool_cmd
-    tool_cmd="nuclei --version"
-    test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+    test -x "$install_path/nuclei" || fail "Expected $install_path/nuclei to be executable."
 
     echo "nuclei $version installation was successful!"
   ) || (
